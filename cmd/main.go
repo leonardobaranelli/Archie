1fbda07e2da0beba3730336967ff292e3121e914
leonardobaranelli <leobaranelli@gmail.com>
2024-03-05 16:17:09 -0300

    feat: air (live refresh tool)

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Server on")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Server on")
+	})
 
-    app.Listen(":3000")
+	app.Listen(":3000")
 }
